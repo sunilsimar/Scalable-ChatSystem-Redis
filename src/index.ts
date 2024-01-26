91487d39eb54ca45e2293ad38e94d538ccd96c0f
Sunil <sunisimar2002d@gmail.com>
2024-01-26 12:37:58 +0530

    created webSocket-Server

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const PORT = 3000;
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+const users: {
+  [key: string]: {
+    room: string;
+    ws: any;
+  };
+} = {};
+
+let counter = 0;
+
+wss.on("connection", async (ws, req) => {
+  const wsId = counter++;
+  ws.on("message", (message: string) => {
+    const data = JSON.parse(message.toString());
+    if (data.type === "join") {
+      users[wsId] = {
+        room: data.payload.roomId,
+        ws,
+      };
+      RedisSubscriptionManager.getInstance().subscribe(
+        wsId.toString(),
+        data.payload.roomId,
+        ws
+      );
+    }
+
+    if (data.type == "message") {
+      const roomId = users[wsId].room;
+      const message = data.payload.message;
+      RedisSubscriptionManager.getInstance().addChatMessage(roomId, message);
+    }
+  });
+
+  ws.on("disconnect", () => {
+    RedisSubscriptionManager.getInstance().unsubscribe(
+      wsId.toString(),
+      users[wsId].room
+    );
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server is started");
+});
